dc7ac271c6d8838a5a16f8ec64af96430e0f924c
aehmt <calisahmet@gmail.com>
2016-08-30 17:13:20 -0400

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,27 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.motorboaters
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+
+  def self.catamaran_operators
+    all.joins(boats: :classifications).where("classifications.name = 'Catamaran'").group("captains.id")
+  end
+
+  def self.sailors
+    all.joins(boats: :classifications).where("classifications.name = 'Sailboat'").group("captains.id")
+  end
+
+  def self.talented_seamen
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+  end
+  
+  def self.non_sailors
+    where("id NOT IN (?)", self.sailors.pluck(:id))
+  end
 end
--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    all.group("classifications.id")
+  end
+
+  def self.longest
+    Boat.all.joins(:classifications).order("length DESC").limit(1)[0].classifications
+  end
 end
